2fa4cb9004c32a3769715b4c3b9a906c86096282
jacyjd <168132595+jacyjd@users.noreply.github.com>
2024-05-10 14:54:58 -0400

    Add files via upload
    Added my files to github

---
+++ project/README.md
@@ -0,0 +1,23 @@
+# Guessing Country Names!
+
+In this game, two players will go back and forth guessing a letter that might be in the mystery word. This game is soon-to-be in the New York Times Games, where each day the mystery topic is a secret for players. Today's theme is name of a country...however players do not know this. The fun of this daily game is for players to race their friends guessing a random mystery word with an unknown topic. After a certain times of failed attempts at guessing the word, the game will end. 
+
+## How to play
+
+Two players will go back and forth guessing letters in the chosen mystery word. After entering in the letter in the mystery word, the program will confirm if that letter is or is not in the mystery word. Turns will flip back and forth between players until they run out of tries or if they correctly guess the word. Each player will receive a point for each letter guess they enter. The program will not tell players where the correctly guessed letter is in the mystery word. The program will tell players how many times the correctly guessed letter is in mystery word. The program will track each player's number of points. The player with the smallest number of letter guesses before correctly guessing the word will win the game. 
+## How to run the program
+
+The Guessing Words Game is used for players to guess a randomized word. As a player you can run the program in VScode.
+
+
+## Copyright
+
+I used GeeksforGeeks at this link https://www.geeksforgeeks.org/python-counter-objects-elements/, in relation to the Counter variable in The Guessing Word Game
+
+## License
+
+MIT 
+
+
+
+
---
+++ project/guessing_word_game.py
@@ -0,0 +1,44 @@
+# Creating a list to hold the word bank
+import random
+from collections import Counter
+person = ["Player #1","Player #2"]
+word_bank = ["Australia", "Mexico", "Armenia", "Albania", "Belize", "Belgium", "Nepal", "Bulgaria", "Norway", "Canada","Cambodia", "Chile", "China", "Panama", "Poland", "Denmark","Russia","Egypt","Serbia","Singapore","Fiji","France", "Sweden","Greece","Ireland", "Israel","Yemen", "Liberia"]
+
+
+# Welcoming message to players and giving them instructions of how to play
+def guess_word(person, word_bank):
+    print("Hello players! You are now a player of my mystery guessing game. Each time you pick if you want to guess a letter in the word, or the word itself. You get 3 tries to guess the word and turns alternate. Each time you guess a letter that's one point added to your score. Lowest score wins. ")
+   
+# Creating a loop where the program selects a word from the bank and loops from player to player to make a letter guess.
+    for current_player in person: 
+        chosen_word = random.choice(word_bank)
+        tries_left = 3
+        letter_guess = Counter()
+        correct_guess = False
+# This keeps track of the number of guesses each player makes, also it allows the players to continue guessing until they run out of tries. This is when it prompts players for a letter input.
+        while tries_left > 0:
+            print("Number of tries left:",tries_left)
+            counting_letter = input("Guess a letter that you think is in the word:").lower()
+            times_used = chosen_word.lower().count(counting_letter)
+            letter_guess[current_player] += 1
+# This tells the player if their guess is in the chosen word.
+            if counting_letter in chosen_word.lower():
+                print (f"Good job! You are right. The number of times that letter is in the word is '{times_used}' times")
+            else:
+                print("No. I'm sorry...that's not correct.")
+            tries_left -=1
+            word_guess = input("Enter the word you believe is our mystery word: ").lower() 
+# This is what happens when the player runs out of tries. Here they must guess the word and they are told if they are correct or not.      
+            if word_guess == chosen_word.lower():
+                print(f"Good job! You guessed the word '{chosen_word}' ")
+                correct_guess = True
+                break
+            else:
+                print("No I'm sorry... that is not correct.")
+# This prints the ending score if a player runs out of tries and guesses incorrectly.
+        if not correct_guess:
+            ending_game =  letter_guess[current_player]
+            print(f"The ending score is {current_player}: {ending_game}")
+
+guess_word(person, word_bank)
+
